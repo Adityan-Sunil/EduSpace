c2e084694915ccbb12eb7d00f2733daec382e88a
Interagus <adityan.sk@hotmail.com>
2021-02-17 17:58:01 +0530

    almost_complete

--- Static/login.js
+++ Static/login.js
@@ -1,3 +1,66 @@
+var userID;
+var tdna = new TypingDNA();
+
+//Adding typingPattern Targets
+tdna.addTarget('test-email');
+tdna.addTarget('test-name');
+tdna.addTarget('test-password');
+tdna.addTarget('typedna-test');
+tdna.addTarget('email');
+tdna.addTarget('pwd');
+
+var startTime;
+$("#typing-test").hide();
+$("#test-switch").click(function(){
+    tdna.stop();
+    var length = $("#test-email").val().length + $("#test-password").val().length + $("#test-name").val().length;
+    if(length <=0){
+        $("#test-email").placeholder = "Enter Email";
+    }else{
+        
+        var tpEmail = tdna.getTypingPattern({type:0,length:$('#test-email').val().length, targetId: 'test-email'});
+        var tpPass = tdna.getTypingPattern({type:0,length:$('#test-password').val().length, targetId: 'test-password'});
+        var tpName = tdna.getTypingPattern({type:0,length:$('#test-name').val().length, targetId: 'test-name'});
+        var params =  {
+            "email":$('#test-email').val(),
+            "tp":[tpEmail,tpPass,tpName],
+            "password":$('#test-password').val(),
+            "name":$('#test-name').val()
+        }
+        ajaxCall(params,function(err){
+            console.log(err);
+        },"signup")
+    }
+    $("#user-dets").hide();
+    $("#typing-test").show();
+})
+$("#typedna-test").keyup(function(){
+    var length = $("#typedna-test").val().length;
+    var wpm = length*23500/(new Date().getTime() - startTime);
+    $("#wpm").css("width", wpm)
+})
+$("#typedna-test").focus(function(){
+    tdna.start(); //start tracking
+    startTime = new Date().getTime();
+    console.log("Area on focus");
+})
+$("#code-submit").click(function(){
+    tdna.stop();
+    console.log($("#typedna-test").val());
+    var params = {
+        action:"enroll",
+        tp:tdna.getTypingPattern({
+            type:1,
+            length:$("#typedna-test").val().length, 
+            text:$("#test-code").text(), 
+            targetId:'typedna-test'
+        })
+    }
+    ajaxCall(params,function(error){
+        console.log(error)
+    }, "enroll")
+})
+
 var btn_switch = document.getElementsByClassName('switch-btn');
 var signupForm = true;
 for (let i = 0; i < btn_switch.length; i++) {
@@ -14,15 +77,50 @@ for (let i = 0; i < btn_switch.length; i++) {
         }
     });
 }
+function checkCookie(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST","http://localhost:3000/getCookie", false);
+    xhttp.setRequestHeader("Content-type","application/json");
+    xhttp.send(JSON.stringify({"password":"password"}));   
+}
+
 $('#login-submit').click(function(e){
     e.preventDefault();
     console.log("Submitted");
+    
+    tdna.stop();
     var email = document.getElementById("login-form").Email.value;
     var password = document.getElementById("login-form").Password.value;
     var xhttp = new XMLHttpRequest();
+    var tpMail = tdna.getTypingPattern({type:0, length: $("#email").val().length+10}, targetId = 'email');
+    var params = {
+        tp:tpMail,
+        action:"enroll"
+    }
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            console.log("Got ID from server");
+            userID = this.responseText;
+            ajaxCall(params,function(error){
+                console.log(error);
+            },"verify");
+        }
+    }
     xhttp.open("POST","http://localhost:3000/login", false);
     xhttp.setRequestHeader("Content-type","application/json");
     console.log(JSON.stringify({"email":email,"password":password}));
     xhttp.send(JSON.stringify({"email":email,"password":password}));   
-    console.log(xhttp.responseText);
 })
+function ajaxCall(params, callback, url){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST","http://localhost:3000/"+url, false);
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 || this.status == 200){
+            callback(this.responseText);
+        }
+    }
+    xhttp.setRequestHeader("Content-type","application/json");
+    console.log(params);
+    xhttp.send(JSON.stringify(params));
+
+}
--- main.js
+++ main.js
@@ -1,17 +1,114 @@
-var express = require('express');
-var app = express();
-var parser = require('body-parser');
+const express = require('express');
+const app = express();
+const axios = require('axios');
+var session = require('express-session');
+const typingDNA = require('typingdnaclient');
+const typingDnaClient = new typingDNA('cc232a43f92f13ac0a736b97a7ed1d86','f777e09c7c4d1976aed3bd9e805f37a6');
 
+app.use(session({secret:'Devpost Hackathon', resave:true, saveUninitialized:false, cookie:{}}));
 app.use(express.json());
 app.use(express.static('Static'));
+app.use(express.urlencoded({
+  extended:true
+}))
 
+
+app.post('/signup',(req,res)=>{
+  console.log("Got Request");
+  //console.log(req.body.tp);
+    axios.interceptors.request.use(config => {
+    console.log('Request was sent');
+    //console.log(config);
+    return config;
+  }, error => {
+    return Promise.reject(error);
+  });
+  axios.post('https://snt1207o90.execute-api.us-east-1.amazonaws.com/development/signup',{
+        email:req.body.email,
+        password:req.body.password
+  },{
+    headers: {
+        'Content-Type': 'application/json',
+    }
+}).then((response)=>{
+    console.log(response.data);
+    req.session.ID = response.data;
+    enrollAPI(req.session.ID, req.body.tp,function(error, result){
+      if(error){console.log(error)}
+      else{console.log(result)}
+    })
+    res.send("success");
+    },(error)=>{
+    console.log(error);
+  })
+
+})
+function verifyAPI(userid, tp, callback){
+  console.log(userid);
+  typingDnaClient.auto(userid,tp,callback);
+}
+function enrollAPI(userid, tp,callback){
+    if(typeof(tp) === "object"){  
+      tp.forEach(function(typingPattern, index){
+        setTimeout(() => {
+          typingDnaClient.auto(userid, typingPattern, callback);
+        },index * 1500);
+    });
+  }else{
+    console.log("sending code");
+    typingDnaClient.auto(userid, tp,callback);
+  }
+}
+app.post('/verify', (req,res)=>{
+  var tp = req.body.tp;
+  var id = req.session.ID;
+  verifyAPI(id, tp, function(err, result){
+    if(err){"Error: ",console.log(err);}
+    else{"Result: ",console.log(result);
+  res.send("check");}
+  })
+})
+app.post('/enroll', (req,res)=>{
+  userId = req.session.ID;
+  console.log(req.body);
+  console.log(req.session.ID);
+  typingPattern = req.body.tp;
+  console.log(typingPattern);
+  enrollAPI(req.session.ID, req.body.tp,function(error,result){
+    if(error){console.log(error);}
+    else{console.log(result);
+      res.send("check");
+    }
+  })
+})
 app.post('/login', (req,res)=>{
     console.log("Got POST");
-    var details = req.body;
-    console.log(details);
-    res.send("Recieved");
-})
-
+    console.log(req.body);
+    var details = (req.body);
+    var resp;
+    axios.interceptors.request.use(config => {
+        console.log('Request was sent'); 
+        return config;
+      }, error => {
+        return Promise.reject(error);
+      });
+      axios.post('https://snt1207o90.execute-api.us-east-1.amazonaws.com/development/login',{
+            email: details.email,
+            password: details.password
+      },{
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }).then((response)=>{
+        resp = response.data;
+        req.session.ID = response.data;
+        console.log("Login: ",req.session);
+        req.session.save();
+        res.send(response.data);
+      },(error)=>{
+        console.log(error);
+      })
+});
 app.listen(3000, function(){
     console.log("Server Listening at 3000\n");
 })
